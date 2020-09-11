c9671643a23f09bd04e499d1b49c7a190d00ea8f
florapetersen <floramarina@comcast.net>
2020-09-11 14:16:57 -0700

    added logged in conditional

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -86,4 +86,11 @@ class RecipesController < ApplicationController
   def logged_in
     !!current_user 
   end
+
+  def redirect_if_not_logged_in
+    if !logged_in?
+      flash[:error] = "You must be logged in to view that page"
+      redirect request.referrer || "/login"
+    end
+  end
 end
