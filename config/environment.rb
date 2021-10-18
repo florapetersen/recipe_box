140f609c9462016e1ee6a11d0f70f1afa51e5fd5
florapetersen <floramarina@comcast.net>
2021-10-18 06:54:12 -0700

    edit environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,7 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+set :database_file, "./database.yml"
 
 Dotenv.load # we generated a "secret" and put it equal to SESSION_SECRET in .env 
 # now we can load the environment variable (SESSION_SECRET), from the .env file,
