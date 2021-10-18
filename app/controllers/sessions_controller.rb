5b70a7e0dfecffe1690eb6a2c832ba9d0291ca5f
florapetersen <floramarina@comcast.net>
2021-10-18 06:42:25 -0700

    remove comments from sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,11 @@ class SessionsController < ApplicationController
     post '/login' do 
         user = User.find_by_email(params[:email])
         if user && user.authenticate(params[:password])
-            session[:id] = user.id # setting the user's id to the session id "logs in" the user
-            redirect "/" # redirect sends a new get request; back to "homepage" from app controller
+            session[:id] = user.id 
+            redirect "/" 
         else
             flash[:error] = "Incorrect email or password" 
-            erb :'/sessions/login' # if can't find user and/or can't authenticate user, displays
-            # error message and directs back to login page
+            erb :'/sessions/login' 
         end
     end
 
